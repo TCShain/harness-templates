e09178a1341cb2552f1dfc05d89c4916c908efae
TCShain <tcshain@gmail.com>
2022-07-20 19:14:19 -0400

    fixing bad api key name

--- ng/terraform/rapid-lab/provider.tf
+++ ng/terraform/rapid-lab/provider.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "harness" {
-  endpoint   = "https://app.harness.io/gateway"
-  account_id = var.harness_account_id
-  api_key    = var.harness_api_key
+  endpoint            = "https://app.harness.io/gateway"
+  account_id          = var.harness_account_id
+  platform_api_key    = var.harness_api_key
 }
