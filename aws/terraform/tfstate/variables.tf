dc80b3f617b8ecf816ec2019c00674e28fc101ff
TCShain <tcshain@gmail.com>
2023-04-11 14:28:06 -0400

    Update variables.tf

--- aws/terraform/tfstate/variables.tf
+++ aws/terraform/tfstate/variables.tf
@@ -1,14 +1,17 @@
 ## ID Variables
 variable "namespace" {
     type = string
+    default = "demo"
 }
 
 variable "stage" {
     type = string
+    default = "stage"
 }
 
 variable "name" {
     type = string
+    default = "app"
 }
 
 ## AWS Variables
