a296ee21f5386b1e661484a5741ef16e2df84acc
TCShain <tcshain@gmail.com>
2023-04-13 10:12:32 -0400

    Update variables.tf

--- aws/terraform/tfstate/variables.tf
+++ aws/terraform/tfstate/variables.tf
@@ -19,3 +19,13 @@ variable "region" {
   type        = string
   description = "AWS region"
 }
+
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Access Key from Harness Pipeline input"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Secret Key from Harness Pipeline input"
+}
